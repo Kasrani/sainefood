d5c122a94af65dcf189a54cab48be17fcaa22e3e
Kasrani <kasrani.mourad@gmail.com>
2018-04-22 19:38:20 +0200

    test-google-connect

--- sainefood/assets/js/content.js
+++ sainefood/assets/js/content.js
@@ -1,15 +1,3 @@
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    $('#myFIXED').toggleClass('mystyle',
-     //add 'ok' class when div position match or exceeds else remove the 'ok' class.
-      scroll >= $('#footer').offset().top
-    );
-});
-//trigger the scroll
-$(window).scroll();//ensure if you're in current position when page is refreshed
-
-
-
 
 var height = $('.container-model > .main-content').height();
 console.log(height);
