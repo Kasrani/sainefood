75eb639f89f572ad76da45f5be8897788af875e8
Kasrani <kasrani.mourad@gmail.com>
2018-04-22 17:59:01 +0200

    fiche-produit

--- sainefood/assets/js/main.js
+++ sainefood/assets/js/main.js
@@ -1,1 +1,6 @@
+
+
 $('#recipeCarousel').carousel({\n  interval: 10000\n})\n\n$('.carousel .carousel-item').each(function(){\n    var next = $(this).next();\n    if (!next.length) {\n    next = $(this).siblings(':first');\n    }\n    next.children(':first-child').clone().appendTo($(this));\n    \n    for (var i=0;i<1;i++) {\n        next=next.next();\n        if (!next.length) {\n        \tnext = $(this).siblings(':first');\n      \t}\n        \n        next.children(':first-child').clone().appendTo($(this));\n      }\n});\n
+
+
+                                                                                                        
