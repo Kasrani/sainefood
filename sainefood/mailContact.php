a75f1eb855b191cbb52dd56a28c388078ed9166c
Kasrani <kasrani.mourad@gmail.com>
2018-05-05 17:59:42 +0200

    Gmail SMTP test

--- sainefood/mailContact.php
+++ sainefood/mailContact.php
@@ -25,22 +25,22 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 1;
+$mail->SMTPDebug = 2;
 //Set the hostname of the mail server
-$mail->Host = 'smtp.hotmail.fr';
+$mail->Host = 'smtp.gmail.com';
 // use
 // $mail->Host = gethostbyname('smtp.gmail.com');
 // if your network does not support SMTP over IPv6
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 25;
+$mail->Port = 587;
 //Set the encryption system to use - ssl (deprecated) or tls
 $mail->SMTPSecure = 'ssl';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "kasrani@hotmail.fr";
+$mail->Username = "kasrani.mourad@gmail.com";
 //Password to use for SMTP authentication
-$mail->Password = "aqwzsxedc1234+";
+$mail->Password = "aqwzsxedc123";
 //Set who the message is to be sent from
 $mail->setFrom('kasrani.mourad@gmail.com', 'First Last');
 //Set an alternative reply-to address
