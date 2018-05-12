5f9400ad99dfeb8249a74c9d2abf8ef4a4101098
Kasrani <kasrani.mourad@gmail.com>
2018-05-12 17:55:32 +0200

    ff

--- sainefood/create.php
+++ sainefood/create.php
@@ -60,10 +60,6 @@ $mail->Body = "<body style='width:612px; margin:auto; text-align:center;'>
 <br><br><br>
 <h3 style='color:#484848;text-align:left;'>Cher(e)" .$prenom. "</h3><br>
 <p style='color:#484848;font-size:14px;text-align:left;line-height:20px;'>Votre demande d'ouverture de compte est terminée.</p>
-<br>
-<br>
-<br>
-<br>
 <a style='color:#FF594F;font-size:18px;text-align:left;text-decoration:none;' href='saine-food.fr'>saine-food.fr</a>
 <br><br>
 </center>
