7df52d879086ce254f8a2ae682ced11372ec6cb5
Kasrani <kasrani.mourad@gmail.com>
2018-05-12 18:15:15 +0200

    ss

--- sainefood/create.php
+++ sainefood/create.php
@@ -61,6 +61,7 @@ $mail->Body = "<body style='width:612px; margin:auto; text-align:center;'>
 <p style='color:#484848;font-size:14px;text-align:left;line-height:20px;'>Votre demande d'ouverture de compte est terminée.</p>
 <p style='color:#484848;font-size:14px;text-align:left;line-height:20px;'>A bientôt sur votre Espace Client,</p>
 <h3 style='color:#484848;text-align:left;'>L'équipe Sainefood</h3>
+<hr style='border:0.5px dashed #CFCBC2;'>
 </body>
 ";
 //Read an HTML message body from an external file, convert referenced images to embedded,
