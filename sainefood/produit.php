11782e0ee8a3d332d771056edadbce2692c4d161
Kasrani <kasrani.mourad@gmail.com>
2018-05-12 18:35:18 +0200

    return

--- sainefood/produit.php
+++ sainefood/produit.php
@@ -57,7 +57,7 @@ $row = mysqli_fetch_assoc($result);
             <div class="container">
                 <div class="row headerow">
                     <ul>
-                        <a href="index.php"><span class="icon-retour"></span><span class="retour">Retour</span></a>
+                        <a href="return.php"><span class="icon-retour"></span><span class="retour">Retour</span></a>
                     </ul>
                 </div>
             </div>
