66316170492fe92ccbf706cee8232f1cd461fcdb
Kasrani <kasrani.mourad@gmail.com>
2018-05-13 16:59:16 +0200

    db

--- sainefood/authDB.php
+++ sainefood/authDB.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","bc79c844c05827","11e8e8f1","heroku_a4f632ea2ba8ee3");
+$maConnexion = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","bc79c844c05827","11e8e8f1","heroku_a4f632ea2ba8ee3");
 if (!$maConnexion){
     die("Database Connection Failed" . mysqli_error($maConnexion));
 }
