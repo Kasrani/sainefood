dd5833dfddad5517760f08a9b49aaa7df27d1b2e
Kasrani <kasrani.mourad@gmail.com>
2018-05-15 00:39:27 +0200

    ss

--- sainefood/index.php
+++ sainefood/index.php
@@ -17,7 +17,13 @@ session_start();
             gtag('js', new Date());
             gtag('config', 'UA-119196030-1');
         </script>
-
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M8B7G39');</script>
+        <!-- End Google Tag Manager -->
 		<link rel="stylesheet" href="assets/css/main.css" />
         <link rel="stylesheet" href="icons/style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -39,6 +45,10 @@ session_start();
         
 	</head>
 	<body class="dd">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8B7G39"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div id="header" class="navbar navbar-fixed-top container-fluid">
             <div class="container">
                 <div class="d-flex">
