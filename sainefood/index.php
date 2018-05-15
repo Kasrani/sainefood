de129b73df71ad1edf93c1ed533532270dbc886f
Kasrani <kasrani.mourad@gmail.com>
2018-05-15 21:52:49 +0200

    resp

--- sainefood/index.php
+++ sainefood/index.php
@@ -38,10 +38,6 @@ session_start();
         
 	</head>
 	<body class="dd">
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8B7G39"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <div id="header" class="navbar navbar-fixed-top container-fluid">
             <div class="container">
                 <div class="d-flex">
@@ -143,8 +139,8 @@ session_start();
                     </div>
                     <h6 class="title-sf-5">Programmes à venir (9)</h6>
                     <div class="mx-auto my-auto">
-                        <div id="recipeCarousel" class="carousel slide w-100 scroll" data-ride="carousel">
-                            <div class="carousel-inner w-100 scroll" role="listbox">
+                        <div id="" class="carousel slide w-100 scroll">
+                            <div class="carousel-inner w-100 scroll">
                                 <a class="carousel-control-prev active" href="#recipeCarousel" role="button" data-slide="prev">
                                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span class="sr-only">Previous</span>
@@ -400,6 +396,7 @@ session_start();
         </footer>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="assets/js/main.js"></script>
         <?php 
         
