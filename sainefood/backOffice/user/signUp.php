8d892a4cb48783a21dca84c1455d87526cc929f4
Kasrani <kasrani.mourad@gmail.com>
2018-05-17 00:29:46 +0200

    fix bug signUp

--- sainefood/backOffice/user/signUp.php
+++ sainefood/backOffice/user/signUp.php
@@ -9,7 +9,7 @@ $email=$_POST['email'];
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 $password = $_POST['password'];
-include("authDB.php");
+include("../../authDB.php");
 mysqli_query($maConnexion,"INSERT INTO user (email,prenom,nom,password) VALUES ('$email','$prenom','$nom','$password')") 
 or die(mysqli_error($maConnexion));
 //On envoie un mail de cofirmation
